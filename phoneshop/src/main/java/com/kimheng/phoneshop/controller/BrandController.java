7aed560079d7c83a4c6577378b3b59d3e3349ca7
KimHeng2111 <lengkimheng2111@gmail.com>
2025-12-11 11:29:55 +0700

    Add CROS on brand controller

--- phoneshop/src/main/java/com/kimheng/phoneshop/controller/BrandController.java
+++ phoneshop/src/main/java/com/kimheng/phoneshop/controller/BrandController.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -21,6 +22,7 @@ import com.kimheng.phoneshop.service.BrandService;
 
 @RestController
 @RequestMapping("brands")
+@CrossOrigin(origins = "http://localhost:4200")
 public class BrandController {
 	
 	@Autowired
